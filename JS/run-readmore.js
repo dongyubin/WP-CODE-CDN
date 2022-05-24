70eabcf0662a713d56f9acf15d96fbbabb050ede
dongyubin <892457803@qq.com>
2022-05-24 15:29:03 +0800

    js

---
+++ JS/run-readmore.js
@@ -0,0 +1,8 @@
+const btw = new BTWPlugin();
+    btw.init({
+        id: 'container',
+        blogId: '18755-1653374997523-628',
+        name: '文武科技社',
+        qrcode: 'https://tucang.cc/api/image/show/cc7004c3ed49b65593db26e2c71f383f',
+        keyword: '验证码',
+    });
